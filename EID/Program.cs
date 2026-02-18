cf16dd7aff1278036fd4cb6ecc0425cc9433808e
YyyGuys <bdust@reynolds.edu>
2026-02-17 20:24:47 -0500

    Linq part 1

--- EID/Program.cs
+++ EID/Program.cs
@@ -11,6 +11,7 @@ namespace EID
 
         static void Main(string[] args)
         {
+            #region --< EXTENSION METHOD EXAMPLES >------------------------------------------------------------
             Console.WriteLine("hello".IsCapitalized());
             Console.WriteLine("Bob Dust".Left(3));
             Console.WriteLine("Bob Dust".Right(4));
@@ -23,7 +24,7 @@ namespace EID
             {
                 Console.WriteLine($"ID: {associate.Id}, \tName: {associate.Name}, Total Amount: {associate.TotalAmount}");
             }
-
+            #endregion
             //--< DELEGATE EXAMPLES >------------------------------------------------------------
 
             int[] sevens = new int[]
@@ -31,19 +32,46 @@ namespace EID
                 42, 7, 14, 63, 21, 70, 49, 28, 35, 56
             };
 
-            Numbers math = SumNumbers;
+            Numbers math = Sum;
             int result = math(sevens);
-            Console.WriteLine(result);
+            Console.WriteLine($"The sum is {result}");
+
+            math = Max;
+            result = math(sevens);
+            Console.WriteLine($"The maximum number is {result}");
             Console.ReadKey();
         }
 
         // Method that matches the delegate
-        static int SumNumbers(int[] nums)
+        /// <summary>
+        /// Calculates the sum of all elements in the specified array of integers.
+        /// </summary>
+        /// <param name="nums">An array of integers to be summed. Cannot be null.</param>
+        /// <returns>The total sum of the elements in the array. Returns 0 if the array is empty.</returns>
+        static int Sum(int[] nums)
         {
             int total = 0;
             foreach (var number in nums)
                 total += number;
             return total;
+        }
+        /// <summary>
+        /// Returns the largest value in the specified array of integers.
+        /// </summary>
+        /// <param name="nums">An array of integers to search for the maximum value. Cannot be null or empty.</param>
+        /// <returns>The maximum integer value found in the <paramref name="nums"/> array.</returns>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="nums"/> is null or contains no elements.</exception>
+        static int Max(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                throw new ArgumentException("Input array cannot be null or empty.");
+            int max = nums[0];
+            foreach (var number in nums)
+            {
+                if (number > max)
+                    max = number;
+            }
+            return max;
         }
     }
 }
---
+++ LINQ1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Collection;
+Console.WriteLine("LINQ");
+int[] sevens = new int[]
+            {
+                42, 7, 14, 63, 21, 70, 49, 28, 35, 56
+            };
+//--< Use LINQ >--  Language INtegrated Query
+int sum = sevens.Sum();
+int max = sevens.Max();
+double avg = sevens.Average();
+
+//--< Use Student Class >--//
+var students = Student.Students;
+float gpa = students.Average(s => s.GPA);       //--< => Lambda Expression: s => s.GPA
+Console.WriteLine($"Average GPA: {gpa}");
+double idAvg = students.Average(s => s.StudentId);
+Console.WriteLine($"Average Student ID: {idAvg}");
+
+var deansList = students.Where(s => s.GPA >= 3.0);
+Console.ReadKey();
