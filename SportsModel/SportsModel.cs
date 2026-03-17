3bad7f1237c2f0481cfa934d7979b16dd2b13e90
Bob Dust <BobDust@gmail.com>
2026-03-17 14:32:59 -0400

    Added SportsModel and ADO.NET projects

---
+++ SportsModel/SportsModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SportsModel
+{
+    public partial class Team
+    {
+        public string MyNameIs => "Your Name Here";     //--< Put Your Name Here <<<
+        public Team()
+        {
+            this.Rosters = new HashSet<Roster>();
+        }
+
+        public int TeamId { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public virtual ICollection<Roster> Rosters { get; set; }
+    }
+    public partial class Roster
+    {
+        public int RosterId { get; set; }
+        public int TeamId { get; set; }
+        public int PlayerId { get; set; }
+
+        public virtual Player Player { get; set; }
+        public virtual Team Team { get; set; }
+    }
+    public partial class Player
+    {
+        public Player()
+        {
+            this.Rosters = new HashSet<Roster>();
+        }
+
+        public int PlayerId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public bool IsActivePlayer { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string NickName { get; set; }
+        public bool IsOptInEmail { get; set; }
+        public Nullable<System.DateTime> BirthDate { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+
+        public virtual ICollection<Roster> Rosters { get; set; }
+    }
+}
